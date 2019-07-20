affcf7fd1cbf791f83b9dd647a904976a8fe8ee0
Scarlet <info_ng@me.com>
2019-07-21 04:29:54 +0800

    Initial Commit

---
+++ Streamer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Streamer
+//
+//  Created by Scarlet on A2019/J/21.
+//  Copyright © 2019 Scarlet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
